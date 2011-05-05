0c2472dc5134fd9a0c52b5d9757295b614798832
nguyenluuanhvu@gmail.com <nguyenluuanhvu@gmail.com@09aee3d3-3c22-d711-9e6a-4278868632b9>
2011-05-05 15:55:19 +0000

    Anh Vũ - Chủ Đề

--- trunk/Source/WesiteHoiDap.BUS/ChuDe.cs
+++ trunk/Source/WesiteHoiDap.BUS/ChuDe.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Anh Vũ
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +12,140 @@ namespace WebsiteHoiDap.BUS
     public class ChuDe : SqlDataAccessHelper
     {
         #region Member Variables
+        int intMaChuDe = int.MinValue;
+        string strTenChuDe = String.Empty;
+        int intDaXoa = int.MinValue;
         #endregion
 
         #region Properties
+        public int IntMaChuDe
+        {
+            get { return intMaChuDe; }
+            set { intMaChuDe = value; }
+        }
+        public string StrTenChuDe
+        {
+            get { return strTenChuDe; }
+            set { strTenChuDe = value; }
+        }
+        public int IntDaXoa
+        {
+            get { return intDaXoa; }
+            set { intDaXoa = value; }
+        }
         #endregion
 
         #region method
-        #endregion
+        /// <summary>
+        /// Lấy DS Chủ Đề
+        /// Created by  : Anh Vũ
+        /// Date        : 5/5/2011
+        /// </summary>
+        /// <param name="chuDe"> </param>
+        /// <returns>DataTable dtDSHopDong</returns>
+        
+        public List<ChuDe> LayDSChuDe()
+        {
+            List<ChuDe> lstDSChuDe = new List<ChuDe>();
+            try
+            {
+                DataTable dtDSChuDe = new DataTable();
+                dtDSChuDe = SqlDataAccessHelper.ExecuteQuery("spLayDSChuDe");
+                foreach (DataRow dtRow in dtDSChuDe.Rows)
+                {
+                    ChuDe chuDe = new ChuDe();
+                    chuDe.intMaChuDe = int.Parse(dtRow["MaChuDe"].ToString());
+                    chuDe.strTenChuDe = dtRow["TenChuDe"].ToString();
+                    chuDe.intDaXoa = int.Parse(dtRow["DaXoa"].ToString());
+                    lstDSChuDe.Add(chuDe);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return lstDSChuDe;
+        }
 
+        /// <summary>
+        /// Cập Nhật Chủ Đề
+        /// Created by  : Anh Vũ
+        /// Date        : 5/5/2011
+        /// </summary>
+        /// <param name="chuDe"> </param>
+        /// <returns>1 : 0</returns>
+        
+        public int CapNhatChuDe(ChuDe chuDe)
+        {
+            int res = 0;
+            try
+            {
+                List<SqlParameter> lstParameters = new List<SqlParameter>();
+
+                lstParameters.Add(new SqlParameter("@machude", chuDe.intMaChuDe));
+                lstParameters.Add(new SqlParameter("@tenchude", chuDe.strTenChuDe));
+
+                res = SqlDataAccessHelper.ExecuteNoneQuery("spCapNhatChuDe",lstParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Thêm Chủ Đề
+        /// Created by  : Anh Vũ
+        /// Date        : 5/5/2011
+        /// </summary>
+        /// <param name="chuDe"> </param>
+        /// <returns>1 : 0</returns>
+
+        public int ThemChuDe(string strTenChuDe)
+        {
+            int res = 0;
+            try
+            {
+                List<SqlParameter> lstParameters = new List<SqlParameter>();
+
+                lstParameters.Add(new SqlParameter("@tenchude", strTenChuDe));
+
+                res = SqlDataAccessHelper.ExecuteNoneQuery("spThemChuDe", lstParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Xoá Chủ Đề
+        /// Created by  : Anh Vũ
+        /// Date        : 5/5/2011
+        /// </summary>
+        /// <param name="chuDe"> </param>
+        /// <returns>1 : 0</returns>
+
+        public int XoaChuDe(int iMaChuDe)
+        {
+            int res = 0;
+            try
+            {
+                List<SqlParameter> lstParameters = new List<SqlParameter>();
+
+                lstParameters.Add(new SqlParameter("@tenchude", iMaChuDe));
+
+                res = SqlDataAccessHelper.ExecuteNoneQuery("spXoaChuDe", lstParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return res;
+        }
+
+        #endregion
     }
 }
