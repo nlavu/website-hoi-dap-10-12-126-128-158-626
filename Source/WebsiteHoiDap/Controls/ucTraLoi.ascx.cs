9a89bbb91189c146d934b4e81c098065e29438bd
lethihiep171090 <lethihiep171090@09aee3d3-3c22-d711-9e6a-4278868632b9>
2011-05-10 17:18:58 +0000

    ucCauTraLoi

--- Source/WebsiteHoiDap/Controls/ucTraLoi.ascx.cs
+++ Source/WebsiteHoiDap/Controls/ucTraLoi.ascx.cs
@@ -10,6 +10,8 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using WebsiteHoiDap.BUS;
+using WebsiteHoiDap.BUS.CauHoi;
 
 namespace WebsiteHoiDap.Controls
 {
@@ -17,7 +19,44 @@ namespace WebsiteHoiDap.Controls
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            frmKetQuaTraLoi.Visible = false;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            WebsiteHoiDap.BUS.CauHoi cauHoi = new WebsiteHoiDap.BUS.CauHoi();
+            WebsiteHoiDap.BUS.CauTraLoi cauTraLoi = new WebsiteHoiDap.BUS.CauTraLoi();
+
+            cauHoi.NoiDungCauHoi = txtCauHoi.Text;
+            cauHoi.GhiChu = txtGhiChu.Text;
+            cauTraLoi.NoiDung = txtCauTraLoi.Text;
+ 
+            if (cauHoi.NoiDungCauHoi == "")
+            {
+                frmKetQuaTraLoi.Visible = true;
+                lblKetQuaTraLoi.Text = "Chưa nhập nội dung câu hỏi!";
+            }
+
+            else
+            {
+                int kq;
+                kq = cauTraLoi.ThemCauTraLoi();
+                if (kq == 1)
+                {
+                    frmKetQuaTraLoi.Visible = true;
+                    lblKetQuaTraLoi.Text = "Đăng câu trả lời thành công!";
+                    frmTraLoiCauHoi.Visible = false;
+                }
+                else
+                {
+                    frmKetQuaDatTraLoi.Visible = true;
+                    lblKetQuaTraLoi.Text = "Đăng câu trả lời bị lỗi";
+                }
+
+            }
+
 
         }
+   
     }
 }
